d083a786aabc1505df3b296eadb7333a02f3f447
kata-n <kata.kawa813@gmail.com>
2020-04-14 08:48:05 +0900

    twitter

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/mainpage';
 
     /**
      * Create a new controller instance.
