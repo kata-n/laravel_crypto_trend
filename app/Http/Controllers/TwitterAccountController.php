3cae98ddf79ef7b92e38049561b24a20e60fa975
kata-n <kata.kawa813@gmail.com>
2020-04-15 19:53:50 +0900

    twitter

--- app/Http/Controllers/TwitterAccountController.php
+++ app/Http/Controllers/TwitterAccountController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
 
 class TwitterAccountController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        //ツイートを5件取得
+        $result = \Twitter::get('statuses/home_timeline', array("count" => 5));
+
+        //ViewのTwitter.blade.phpに渡す
+        return view('main_page/main_page', [
+            "result" => $result
+        ]);
+    }
 }
