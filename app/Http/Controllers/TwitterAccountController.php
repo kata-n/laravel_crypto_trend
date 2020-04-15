edb1fb59fd415dfca3fbaf05b9568e1091cb050c
kata-n <kata.kawa813@gmail.com>
2020-04-15 20:33:12 +0900

    twitter

--- app/Http/Controllers/TwitterAccountController.php
+++ app/Http/Controllers/TwitterAccountController.php
@@ -13,9 +13,9 @@ class TwitterAccountController extends Controller
 //        //ツイートを5件取得
 //        $result = \Twitter::get('statuses/home_timeline', array("count" => 5));
 //
-//        //ViewのTwitter.blade.phpに渡す
-//        return view('main_page/main_page', [
-//            "result" => $result
-//        ]);
+        //ViewのTwitter.blade.phpに渡す
+        return view('main_page/main_page', [
+            "result" => $result
+        ]);
     }
 }
