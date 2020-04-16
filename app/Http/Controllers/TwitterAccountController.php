74a184e00be4d4ec881d2860d2c6c5ad62796bf9
kata-n <kata.kawa813@gmail.com>
2020-04-16 12:46:46 +0900

    twitter

--- app/Http/Controllers/TwitterAccountController.php
+++ app/Http/Controllers/TwitterAccountController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Laravel\Socialite\Facades\Socialite;
+//use Laravel\Socialite\Facades\Socialite;
 
 class TwitterAccountController extends Controller
 {
