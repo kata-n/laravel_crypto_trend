a73b07dcf7797b51ea9cba3e4ed73a9c30604ebc
kata-n <kata.kawa813@gmail.com>
2020-04-17 19:12:39 +0900

    twitter

--- app/Providers/TwitterServiceProvider.php
+++ app/Providers/TwitterServiceProvider.php
@@ -40,4 +40,12 @@ class TwitterServiceProvider extends ServiceProvider
     {
         //
     }
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['twitter'];
+    }
 }
