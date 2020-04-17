c3894346e67b3a40119182732a89f1c66dd1c473
kata-n <kata.kawa813@gmail.com>
2020-04-17 19:01:58 +0900

    twitter

--- app/Http/Controllers/TwitterAccountController.php
+++ app/Http/Controllers/TwitterAccountController.php
@@ -4,19 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-//use Laravel\Socialite\Facades\Socialite;
+use Abraham\TwitterOAuth\TwitterOAuth;
 
 class TwitterAccountController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-//        //ツイートを5件取得
-//        $result = \Twitter::get('statuses/home_timeline', array("count" => 5));
-//
-//        //ViewのTwitter.blade.phpに渡す
-//        return view('main_page/main_page', [
-//            "result" => $result
-//        ]);
-          return view('main_page/main_page');
+        //ツイートを5件取得
+        $result = \Twitter::get('statuses/home_timeline', array("count" => 5));
+
+        //ViewのTwitter.blade.phpに渡す
+        return view('main_page/main_page', [
+            "result" => $result
+        ]);
+
     }
 }
---
+++ app/Providers/TwitterServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+class TwitterServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+      $this->app->singleton('twitter', function () {
+
+          $config = config::get('services.twitter');
+
+          return new TwitterOAuth($config['api_key'], $config['secret_key'], $config['access_token'], $config['access_token_secret']);
+
+      });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
