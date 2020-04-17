90d52a2d128dfe809de0f67b31a8671ba2d1ff09
kata-n <kata.kawa813@gmail.com>
2020-04-17 19:20:03 +0900

    twitter

--- resources/views/top_page/top_page.blade.php
+++ resources/views/top_page/top_page.blade.php
@@ -22,6 +22,14 @@
       <div id="app">
         <toppage-component></toppage-component>
       </div>
+                 <a class="dropdown-item" href="{{ route('logout') }}"
+        onclick="event.preventDefault();
+        document.getElementById('logout-form').submit();">
+        {{ __('Logout') }}
+      </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+      </form>
   </section>
 @endsection
 
