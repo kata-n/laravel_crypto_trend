debd437857852d7e735a84d870679797d0f819d7
kata-n <kata.kawa813@gmail.com>
2020-04-18 12:35:45 +0900

    twitter

--- app/Http/Controllers/TwitterAccountController.php
+++ app/Http/Controllers/TwitterAccountController.php
@@ -12,7 +12,7 @@ class TwitterAccountController extends Controller
     {
 
       $config = config('services');
-require_once('path/to/twitteroauth.php');
+
       $twitter = new TwitterOAuth($config['api_key'], $config['secret_key'], $config['access_token'], $config['access_token_secret']);
 
         //ツイートを5件取得
