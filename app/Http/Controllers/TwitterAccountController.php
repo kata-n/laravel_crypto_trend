92aecb3fe8597f26abf4c73afbdbbd961c876e64
kata-n <kata.kawa813@gmail.com>
2020-04-18 12:14:15 +0900

    twitter

--- app/Http/Controllers/TwitterAccountController.php
+++ app/Http/Controllers/TwitterAccountController.php
@@ -5,14 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Abraham\TwitterOAuth\TwitterOAuth;
-use App\Facades\Twitter;
 
 class TwitterAccountController extends Controller
 {
     public function index(Request $request)
     {
+
+      $config = config('services.twitter');
+
+      $twitter = new TwitterOAuth($config['api_key'], $config['secret_key'], $config['access_token'], $config['access_token_secret']);
+
         //ツイートを5件取得
-        $result = \Twitter::get('statuses/home_timeline', array("count" => 5));
+        $result = $twitter->get('statuses/home_timeline', array("count" => 5));
 
         //ViewのTwitter.blade.phpに渡す
         return view('main_page/main_page', [
