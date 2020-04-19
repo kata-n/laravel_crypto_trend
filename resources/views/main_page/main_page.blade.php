e1f9c7f9bf76143c5253803e90a7e2359ff5e413
kata-n <kata.kawa813@gmail.com>
2020-04-19 18:18:39 +0900

    twitter

--- resources/views/main_page/main_page.blade.php
+++ resources/views/main_page/main_page.blade.php
@@ -29,6 +29,18 @@
         {{ __('Logout') }}
       </a>
 
+        @foreach ($result as $tweet)
+            <div class="card mb-2">
+                <div class="card-body">
+                    <div class="media">
+                        <div class="media-body">
+                            <h5 class="d-inline mr-3"><strong>{{ $tweet->user->name }}</strong></h5>
+                            <p class="mt-3 mb-0">{{ $tweet->text }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
 
       <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
         @csrf
