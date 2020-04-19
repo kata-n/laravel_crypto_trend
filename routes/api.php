03f7276ebcf6171b4a58bc86a6b94a9c8a582d8c
kata-n <kata.kawa813@gmail.com>
2020-04-20 07:14:46 +0900

    twitter

--- routes/api.php
+++ routes/api.php
@@ -18,30 +18,3 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
- Route::get("/data", function(){
-  $twitter = new TwitterOAuth(
-      config('services.twitter.client_id'),
-      config('services.twitter.client_secret'),
-      config('services.twitter.access_token'),
-      config('services.twitter.access_token_secret')
-  );
-        //パラメータ
-        $params = array(
-            "q" => "仮想通貨",
-            "lang" => "ja",
-            "locale" => "ja",
-            "count" => "6",
-    //      "until" => "2020-01-01",
-    //      "since_id" => "643299864344788992",
-    //      "max_id" => "643299864344788992",
-            "include_entities" => "true",
-        );
-  $sourceUrl = "https://api.twitter.com/1.1/search/tweets.json";
-  $responseData = $twitter -> request("GET", $sourceUrl, $params);
-  $responseBody = json_decode($responseData -> getBody() -> getContents(), true);
-  return [
-   "status" => "OK",
-   "data" => $responseBody,
-  ];
- });
