67e915f5c548e6a34c8c93ce532202e8ba7e3337
kata-n <kata.kawa813@gmail.com>
2020-04-22 18:35:13 +0900

    twitter

--- resources/js/components/AccountpageComponent.vue
+++ resources/js/components/AccountpageComponent.vue
@@ -4,9 +4,8 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">Twitterアカウントのページです</div>
-                    <a href="/mainpage" class="">メインページへ</a>
-
                     <dl v-for="(account in accountdata.results">
+                      <button type="submit" @click.stop.prevent="changefollow()">フォローする</button>
                       <dt>アカウント名：{{account.name}}</dt>
                       <dt>ユーザー名：{{account.screen_name}}</dt>
                       <dt>フォロー数：{{account.friends_count}}</dt>
@@ -14,6 +13,7 @@
                       <dt>プロフィール：{{account.description}}</dt>
                       <dt>最新ツイート：{{account.status.text}}</dt>
                     </dl>
+                    <a href="/mainpage" class="">メインページへ</a>
                 </div>
             </div>
         </div>
@@ -24,13 +24,32 @@
     export default {
         data() {
           return {
-            accountdata :[]
+            accountdata :[],
+            isFollowedBy : {
+              type: Boolean,
+              default: false,
+            }
           };
         },
         mounted() {
           this.$http.get("/twitteraccount")
           .then(response => {this.accountdata = response.data;
           });
+        },
+        methods: {
+          changefollow() {
+            this.isFollowedBy
+            ? this.ubfollow()
+            : this.follow()
+          },
+          async follow() {
+            const response = await axios.post('https://api.twitter.com/1.1/friendships/create.json');
+            this.isFollowedBy = true
+          },
+          async unfollow() {
+            const response = await axios.delete(this.endpoint)
+            this.isFollowedBy = false
+          },
         }
     }
 </script>
