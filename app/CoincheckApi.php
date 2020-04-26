eb14a5d3efb16aced1d99e107f5fc723c71c288a
kata-n <kata.kawa813@gmail.com>
2020-04-26 20:40:04 +0900

    coin

---
+++ app/CoincheckApi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CoincheckApi extends Model
+{
+  //SQLのテーブル名を指定
+  protected $table = 'crypto_detail';
+}
--- app/Http/Controllers/CoincheckController.php
+++ app/Http/Controllers/CoincheckController.php
@@ -4,19 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use Illuminate\Support\Facades\DB;
+use App\CoincheckApi;
 
 class CoincheckController extends Controller
 {
   public function ticker(Request $request)
   {
 
-      $strUrl = "https://coincheck.com/api/ticker";
-      $file = file_get_contents($strUrl);
-      $file = mb_convert_encoding($file, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-      $result = json_decode($file,true);
+    $strUrl = "https://coincheck.com/api/ticker";
+    $file = file_get_contents($strUrl);
+    $file = mb_convert_encoding($file, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+    $result = json_decode($file,true);
+
+    //ビットコインの価格を更新
+    $cryptodb_BTC = \App\CoincheckApi::where('crypto_id', 1)->first();
+    $cryptodb_BTC->crypto_high = $result['high'];
+    $cryptodb_BTC->crypto_low = $result['low'];
+    $cryptodb_BTC->save();
 
     //jsonにてVueに渡す
-    return response()->json(['results' => $result["high","low"]]);
+    return response()->json(['results' => $result]);
   }
 }
