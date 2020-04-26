1d2fcecc9eb644da75e0757c591a7e8deb8cca24
kata-n <kata.kawa813@gmail.com>
2020-04-26 19:05:32 +0900

    coincheck

--- app/Http/Controllers/CoincheckController.php
+++ app/Http/Controllers/CoincheckController.php
@@ -13,7 +13,7 @@ class CoincheckController extends Controller
 
     $coincheck= new Coincheck(
       config('services.coincheck.access_key'),
-      config('services.coincheck.secret_key'),
+      config('services.coincheck.secret_key')
     );
 
     $result = $coincheck->ticker->all();
