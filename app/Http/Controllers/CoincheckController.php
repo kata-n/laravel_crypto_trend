66118db7a4a94b7c10746dcfa7706b1d9b95f77e
kata-n <kata.kawa813@gmail.com>
2020-04-26 19:10:28 +0900

    twitter

--- app/Http/Controllers/CoincheckController.php
+++ app/Http/Controllers/CoincheckController.php
@@ -4,19 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Coincheck\Coincheck;
+
 
 class CoincheckController extends Controller
 {
   public function ticker(Request $request)
   {
 
-    $coincheck= new Coincheck(
-      config('services.coincheck.access_key'),
-      config('services.coincheck.secret_key')
-    );
-
-    $result = $coincheck->ticker->all();
+      $strUrl = "https://coincheck.com/api/ticker";
+      $file = file_get_contents($strUrl);
+      $file = mb_convert_encoding($file, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+      $result = json_decode($file,true);
 
     //jsonにてVueに渡す
     return response()->json(['results' => $result]);
