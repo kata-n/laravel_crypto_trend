c14bb07c34028e5bac797ec935f001642b258788
kata-n <kata.kawa813@gmail.com>
2020-05-02 09:41:16 +0900

    tweetcount

--- app/Http/Controllers/TweetcountController.php
+++ app/Http/Controllers/TweetcountController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use App\CoincheckApi;
+use Abraham\TwitterOAuth\TwitterOAuth;
 
 class TweetcountController extends Controller
 {
