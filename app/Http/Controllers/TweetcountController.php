9cfa6f26bc62718b349e5b6dd23561402ca9215f
kata-n <kata.kawa813@gmail.com>
2020-05-03 17:58:55 +0900

    twitter

--- app/Http/Controllers/TweetcountController.php
+++ app/Http/Controllers/TweetcountController.php
@@ -30,7 +30,7 @@ class TweetcountController extends Controller
 
         //検索クエリ指定
         $params = array(
-            "q" => $value2,
+            "q" => $key2,
             "lang" => "ja",
             "locale" => "ja",
             "count" => "4",
