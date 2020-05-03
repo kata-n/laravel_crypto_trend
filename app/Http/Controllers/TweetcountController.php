cd58cfcf0a315f8e245c3643e886ce762a0477cd
kata-n <kata.kawa813@gmail.com>
2020-05-03 18:34:13 +0900

    twitter

--- app/Http/Controllers/TweetcountController.php
+++ app/Http/Controllers/TweetcountController.php
@@ -24,7 +24,7 @@ class TweetcountController extends Controller
     //DBから銘柄を取得する
     $cryptos = \App\CoincheckApi::select('crypto_id','name','name_ja')->get()->toArray();
 
-    foreach($crypto_list as $crypto) {
+    foreach($cryptos as $crypto) {
 
         //検索クエリ指定
         $params = array(
