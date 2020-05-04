4a50c63a57a412582d057db40cf7f4dfa6693ecd
kata-n <kata.kawa813@gmail.com>
2020-05-04 19:40:53 +0900

    t

--- app/Http/Controllers/TweetcountController.php
+++ app/Http/Controllers/TweetcountController.php
@@ -36,7 +36,7 @@ class TweetcountController extends Controller
               "q" => $value["name"].'+#'.$value["name_ja"].' -rt -bot',
               "lang" => "ja",
               "locale" => "ja",
-              "count" => "4",
+              "count" => "50",
               "include_entities" => "false",
           );
 
