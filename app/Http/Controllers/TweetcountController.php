a9e5a3862b8dd2f5d261d548e2d3f2059971b5ee
kata-n <kata.kawa813@gmail.com>
2020-05-04 17:49:21 +0900

    twitter

--- app/Http/Controllers/TweetcountController.php
+++ app/Http/Controllers/TweetcountController.php
@@ -26,18 +26,34 @@ class TweetcountController extends Controller
 
     foreach($cryptos as $crypto => $value) {
 
+      //一階につき100件までしか取得できない為、ループ処理する
+      $request_loop = 2
+
+      for($i=0; $i<$request_loop; $i++){
+
         //検索クエリ指定
         $params = array(
             "q" => $value["name"].'+#'.$value["name_ja"].' -rt -bot',
             "lang" => "ja",
             "locale" => "ja",
-            "count" => "10",
+            "count" => "2",
             "include_entities" => "false",
         );
 
       $result = $twitter->get('search/tweets', $params);
 
       $tweet_results[] = $result;
+
+      // これ以上取得できるツイートがあるか条件分岐
+      if(isset($results->search_metadata->next_results)){
+         // 次ページURLのmax_id値を取得
+         $max_id = preg_replace('/.*?max_id=([\d]+)&.*/', '$1', $results->search_metadata->next_results);
+         // あればmax_idをparamsに追加
+         $params['max_id'] = $max_id;
+      }else{
+         break;
+      }
+    }
   }
 
 
