87862d48b961d613173edeff828dfe52d13a5bdf
kata-n <kata.kawa813@gmail.com>
2020-05-07 15:07:05 +0900

    t

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \App\Console\Commands\CoincheckCommand::class,
+        \App\Console\Commands\TweetCountCommand::class,
     ];
 
     /**
