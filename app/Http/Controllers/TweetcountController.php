9cdbb3921ee56e10f56feb09c6c85ece51ca99d5
kata-n <kata.kawa813@gmail.com>
2020-05-07 14:05:08 +0900

    t

--- app/Http/Controllers/TweetcountController.php
+++ app/Http/Controllers/TweetcountController.php
@@ -55,7 +55,7 @@ class TweetcountController extends Controller
            // max_idをparamsに追加
            $params["max_id"] = $max_id;
           //配列化
-           $tweet_results[] = count(array($results));
+           $tweet_results[] = count(array($results),1);
 //           $tweet_results[] = $results;
         }else{
            break;
