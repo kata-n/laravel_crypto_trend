e99973fb39537791365e1d5eeb02a47bd4ef6cc8
kata-n <kata.kawa813@gmail.com>
2020-05-08 19:28:18 +0900

    la ja

--- resources/lang/ja/auth.php
+++ resources/lang/ja/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Eメールまたはパスワードが間違っています',
+    'throttle' => 'パスワードが一致しません。しばらく経ってからやり直してください',
 
 ];
