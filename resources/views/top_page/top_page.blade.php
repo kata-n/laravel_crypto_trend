834fb35987a631f5c25f5578e6a9cdc3bb449f83
kata-n <kata.kawa813@gmail.com>
2020-05-09 12:25:06 +0900

    toppage修正

--- resources/views/top_page/top_page.blade.php
+++ resources/views/top_page/top_page.blade.php
@@ -22,15 +22,6 @@
     <div id="app">
       <toppage-component></toppage-component>
     </div>
-
-    <a class="dropdown-item" href="{{ route('logout') }}"
-      onclick="event.preventDefault();
-      document.getElementById('logout-form').submit();">
-      {{ __('Logout') }}
-    </a>
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-    @csrf
-    </form>
   </section>
 @endsection
 
