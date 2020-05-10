de3e92803ae7886c4c26e6d334b53cb415cabeb6
kata-n <kata.kawa813@gmail.com>
2020-05-10 17:21:48 +0900

    t

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -8,9 +8,6 @@ use App\CoincheckApi;
 class RankingController extends Controller
 {
 
-      //primaryKeyの変更
-    protected $primaryKey = "crypto_id";
-
     public function index()
     {
       $hourRnking = CoincheckApi::find(1)->tweetcounts;
--- app/Tweetcount.php
+++ app/Tweetcount.php
@@ -9,6 +9,9 @@ class Tweetcount extends Model
   //SQLのテーブル名を指定
   protected $table = 'tweetcount';
 
+    //primaryKeyの変更
+    protected $primaryKey = "crypto_id";
+
     public function tweet()
     {
         return $this->belongsTo('App\Models\CoincheckApi');
