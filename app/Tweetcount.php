c16114889022878d4046530593fe9767285d6601
kata-n <kata.kawa813@gmail.com>
2020-05-10 17:24:06 +0900

    t

--- app/Tweetcount.php
+++ app/Tweetcount.php
@@ -9,11 +9,4 @@ class Tweetcount extends Model
   //SQLのテーブル名を指定
   protected $table = 'tweetcount';
 
-    //primaryKeyの変更
-    protected $primaryKey = "crypto_id";
-
-    public function tweet()
-    {
-        return $this->belongsTo('App\Models\CoincheckApi');
-    }
 }
