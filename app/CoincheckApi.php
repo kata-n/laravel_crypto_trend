805c59bd7fed1a4e7e0073dcaecb21348d51ec99
kata-n <kata.kawa813@gmail.com>
2020-05-11 00:45:13 +0900

    t

--- app/CoincheckApi.php
+++ app/CoincheckApi.php
@@ -12,7 +12,7 @@ class CoincheckApi extends Model
   //ツイートは複数なので１対多
   public function tweetcounts(){
     return $this->hasMany('App\Tweetcount','crypto_id')
-      ->select('tweet_count', \DB::raw('sum(crypto_id) as total_tweet'))->groupBy('tweet_count');
+      ->select('tweet_count', \DB::raw('sum(crypto_id)'))->groupBy('tweet_count');
   }
 
 }
