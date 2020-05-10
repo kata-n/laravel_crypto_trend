0af9ef939ef5768ce4e796d39bf5adaf814ecbef
kata-n <kata.kawa813@gmail.com>
2020-05-10 19:31:13 +0900

    t

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -10,13 +10,20 @@ class RankingController extends Controller
 
     public function index()
     {
-      $DayRnking = CoincheckApi::with([
-        'tweetcounts' => function($q){
+      $Ranking = CoincheckApi::
+      with(['tweetcounts' => function($q){
         $q->where('created_at', '>', date("Y-m-d", strtotime("-1 day")));
       }])->get();
 
+      $DayRankingData = [
+        'name' => $Rnking('name'),
+        'name_ja' => $Rnking('name_ja'),
+        'crypto_high' => $Rnking('crypto_high'),
+        'crypto_low' => $Rnking('crypto_low'),
+        'tweet_count' => $Rnking->tweetcounts->('tweet_count'),
+      ]
 
-      return ['weekRankingData' => $DayRnking];
+      return ['weekRankingData' => $DayRankingData];
 
     }
 }
