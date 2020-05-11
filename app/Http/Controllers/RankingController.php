8f9feb6b30732d7da283e28f86c93d5a70be2034
kata-n <kata.kawa813@gmail.com>
2020-05-12 00:07:24 +0900

    t

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -15,16 +15,13 @@ class RankingController extends Controller
         $q->where('created_at', '>', date("Y-m-d", strtotime("-1 day")));
       }])->get();
 
-//
-//      $DayRankingData = [
-//        'name' => $Ranking->name,
-//        'name_ja' => $Ranking->'name_ja'),
-//        'crypto_high' => $Ranking('crypto_high'),
-//        'crypto_low' => $Ranking('crypto_low'),
-//        'tweet_count' => $Ranking->tweetcounts->tweet_count
-//      ];
 
-      return ['weekRankingData' => $Ranking];
+      $DayRankingData = [
+
+        'tweet_count' => $Ranking->tweetcounts->sum(tweet_count)
+      ];
+
+      return ['weekRankingData' => $DayRankingData];
 
     }
 }
