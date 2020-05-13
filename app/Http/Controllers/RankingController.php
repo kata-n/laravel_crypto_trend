2fd99bdc8bbbd690245fff284eb8f1aabe0b04cd
kata-n <kata.kawa813@gmail.com>
2020-05-13 20:31:17 +0900

    t

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -13,7 +13,7 @@ class RankingController extends Controller
 
       $Ranking = CoincheckApi::
       with(['tweetcounts' => function($q){
-        $q->whereBetween('created_at', [now()->startOfDay()->format('Y-m-d'), now()->endOfDay()->format('Y-m-d')]);
+        $q->whereBetween('created_at', [now()->startOfWeek()->format('Y-m-d'), now()->endOfWeek()->format('Y-m-d')]);
       }])->get();
 
 //      $Ranking = CoincheckApi::
