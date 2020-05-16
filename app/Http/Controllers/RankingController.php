8073d9be6a8d62a5990e4483ab1d0a20dd6c4697
kata-n <kata.kawa813@gmail.com>
2020-05-16 13:34:14 +0900

    t

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -13,13 +13,16 @@ class RankingController extends Controller
     {
 
 //      過去２４時間
-      $DayRanking = CoincheckApi::
+      $DayCrtptos = CoincheckApi::
       with(['tweetcounts' => function($q){
-        $q->sum("tweet_count")->get();
         $q->whereDate('created_at', date("Y-m-d", strtotime("-1 day")));
       }])->get();
 
-      return ['DayRankingData' => $DayRanking];
+foreach ($DayCrtptos as $DayCrtpto) {
+$sum = array_sum($DayCrtpto=>"tweetcount");
+}
+
+      return ['DayRankingData' => $sum];
 
     }
 }
