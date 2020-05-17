64094bc65b5c90bbac6c94b5c44c7649cde56141
kata-n <kata.kawa813@gmail.com>
2020-05-17 23:45:08 +0900

    t

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -8,9 +8,27 @@ use Illuminate\Support\Collection;
 
 class RankingController extends Controller
 {
-
     public function index()
     {
+      //過去1時間
+      $HourCrtptos = CoincheckApi::
+      with(['tweetcounts' => function($q){
+        $q->whereDate('created_at', date("Y-m-d", strtotime("-1 hour")));
+      }])->get();
+
+      foreach($HourCrtptos as $HourCrtpto => $value){
+         $results['Crypto_name'] = $value['name_ja'];
+         $results['Tweet_count'] = $value->tweetcounts->sum('tweet_count');
+         $results['Tweet_time'] = date("Y-m-d", strtotime("-1 hour"));
+         $results['Crypto_high'] = $value['crypto_high'];
+         $results['Crypto_low'] = $value['crypto_low'];
+         $HourCountresults[] = $results;
+      }
+
+      //ツイート数が多い順に並び替え
+      foreach ((array) $HourCountresults as $key => $value) {
+          $sort[$key] = $value['Tweet_count'];
+      }array_multisort($sort, SORT_DESC, $HourCountresults);
 
       //過去２４時間
       $DayCrtptos = CoincheckApi::
@@ -24,15 +42,39 @@ class RankingController extends Controller
          $results['Tweet_time'] = date("Y-m-d", strtotime("-1 day"));
          $results['Crypto_high'] = $value['crypto_high'];
          $results['Crypto_low'] = $value['crypto_low'];
-         $Countresults[] = $results;
+         $DayCountresults[] = $results;
+      }
+
+      //ツイート数が多い順に並び替え
+      foreach ((array) $DayCountresults as $key => $value) {
+          $sort[$key] = $value['Tweet_count'];
+      }array_multisort($sort, SORT_DESC, $DayCountresults);
+
+
+      //過去一週間
+      $WeekCrtptos = CoincheckApi::
+      with(['tweetcounts' => function($q){
+        $q->whereDate('created_at', date("Y-m-d", strtotime("-7 day")));
+      }])->get();
+
+      foreach($WeekCrtptos as $Weekcrypto => $value){
+         $results['Crypto_name'] = $value['name_ja'];
+         $results['Tweet_count'] = $value->tweetcounts->sum('tweet_count');
+         $results['Tweet_time'] = date("Y-m-d", strtotime("-7 day"));
+         $results['Crypto_high'] = $value['crypto_high'];
+         $results['Crypto_low'] = $value['crypto_low'];
+         $WeekCountresults[] = $results;
       }
 
       //ツイート数が多い順に並び替え
       foreach ((array) $Countresults as $key => $value) {
           $sort[$key] = $value['Tweet_count'];
-      }array_multisort($sort, SORT_DESC, $Countresults);
+      }array_multisort($sort, SORT_DESC, $WeekCountresults);
 
-      return ['DayRankingData' => $Countresults];
-
+      //JSONでVueに渡す
+      return ['DayRankingData' => $HourCountresults,
+              'DayRankingData' => $DayCountresults,
+              'WeekRankingData' => $WeekCountresults,
+             ];
     }
 }
