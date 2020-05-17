07a86a5330b3aa99983f82c39421cdbfee018d47
kata-n <kata.kawa813@gmail.com>
2020-05-17 17:07:14 +0900

    t

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -20,7 +20,7 @@ class RankingController extends Controller
 
     foreach($DayCrtptos as $Daycrypto){
        $results = $Daycrypto->tweetcounts->sum('tweet_count');
-       $results = $Daycrypto->pluck('name_ja');
+       $results = $Daycrypto->each('name_ja');
        $Countresults[] = $results;
     }
 
