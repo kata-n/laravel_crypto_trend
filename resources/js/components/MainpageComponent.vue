adb1eb01ce4b781f51cde03fd37c2097f4d9895e
kata-n <kata.kawa813@gmail.com>
2020-05-19 17:15:09 +0900

    t

--- resources/js/components/MainpageComponent.vue
+++ resources/js/components/MainpageComponent.vue
@@ -68,7 +68,7 @@
           for (var key in array) {
             var cryptoname = array[key].Crypto_name
             self.Crypto_lists.push(cryptoname)
-              array.push({
+              array[key].push({
                 display: true
               });
           }
