539d5a7d1527eca12e4f6f8e6cc3752c8f722777
kata-n <kata.kawa813@gmail.com>
2020-05-24 19:08:42 +0900

    t

--- resources/js/components/MainpageComponent.vue
+++ resources/js/components/MainpageComponent.vue
@@ -12,15 +12,14 @@
             <div>
               <h2>仮想通貨Twitterランキング</h2>
 
-<input type="radio" value="1" id="Tab1" class="Tab-Btn-Radio" v-model="isActive">
-<label for="Tab1" class="Tab-Btn-Label">過去1時間</label>
+              <input type="radio" value="1" id="Tab1" class="Tab-Btn-Radio" v-model="isActive">
+              <label for="Tab1" class="Tab-Btn-Label">過去1時間</label>
 
-<input type="radio" value="2" id="Tab2" class="Tab-Btn-Radio" v-model="isActive">
-<label for="Tab2" class="Tab-Btn-Label">過去24時間</label>
+              <input type="radio" value="2" id="Tab2" class="Tab-Btn-Radio" v-model="isActive">
+              <label for="Tab2" class="Tab-Btn-Label">過去24時間</label>
 
-<input type="radio" value="3" id="Tab3" class="Tab-Btn-Radio" v-model="isActive">
-<label for="Tab3" class="Tab-Btn-Label">過去一週間</label>
-
+              <input type="radio" value="3" id="Tab3" class="Tab-Btn-Radio" v-model="isActive">
+              <label for="Tab3" class="Tab-Btn-Label">過去一週間</label>
             </div>
 
               <div>
@@ -36,23 +35,23 @@
               </div>
               <p>選択している仮想通貨：{{ preview }}</p>
 
-              <div v-for="Weekdata in displayWeekDatas" v-if="isActive == '1'">
-                <a v-bind:href="'https://twitter.com/search?q=' + Weekdata.Crypto_name" target="_blank">
-                {{ Weekdata.Crypto_name }}
-              </a>
-              <p>ツイート数：{{ Weekdata.Tweet_count }}</p>
-              </div>
-              <div v-for="Hourdata in displayHourDatas" v-if="isActive == '2'">
+              <div v-for="Hourdata in displayHourDatas" v-if="isActive == '1'">
                 <a v-bind:href="'https://twitter.com/search?q=' + Hourdata.Crypto_name" target="_blank">
                 {{ Hourdata.Crypto_name }}
               </a>
               <p>ツイート数：{{ Hourdata.Tweet_count }}</p>
               </div>
-              <div v-for="Daydata in displayDayDatas" v-if="isActive == '3'">
+              <div v-for="Daydata in displayDayDatas" v-if="isActive == '2'">
                 <a v-bind:href="'https://twitter.com/search?q=' + Daydata.Crypto_name" target="_blank">
                 {{ Daydata.Crypto_name }}
               </a>
               <p>ツイート数：{{ Daydata.Tweet_count }}</p>
+              </div>
+              <div v-for="Weekdata in displayWeekDatas" v-if="isActive == '3'">
+                <a v-bind:href="'https://twitter.com/search?q=' + Weekdata.Crypto_name" target="_blank">
+                {{ Weekdata.Crypto_name }}
+              </a>
+              <p>ツイート数：{{ Weekdata.Tweet_count }}</p>
               </div>
 
         </div>
@@ -93,7 +92,7 @@
           return this.Weekdatas.filter(weekData => this.preview.includes(weekData.Crypto_name));
         },
         displayDayDatas() {
-          return this.Hourdatas.filter(Daydata => this.preview.includes(Daydata.Crypto_name));
+          return this.Daydatas.filter(Daydata => this.preview.includes(Daydata.Crypto_name));
         },
         displayHourDatas() {
           return this.Hourdatas.filter(Hourdata => this.preview.includes(Hourdata.Crypto_name));
