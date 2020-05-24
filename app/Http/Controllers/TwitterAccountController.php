e883e0ee22b51025da4ced1e3cc3037b95573b45
kata-n <kata.kawa813@gmail.com>
2020-05-24 10:48:38 +0900

    t

--- app/Http/Controllers/TwitterAccountController.php
+++ app/Http/Controllers/TwitterAccountController.php
@@ -64,11 +64,11 @@ class TwitterAccountController extends Controller
       $auto_flg = Auth::user()->aotofollow_flg;
 
       if($auto_flg === 1){
-          $user->aotofollow_flg = true;
+          $user->aotofollow_flg = 0;
           $user->save();
           $auto_flg = 0;
       } else {
-          $user->aotofollow_flg = false;
+          $user->aotofollow_flg = 1;
           $user->save();
           $auto_flg = 1;
       }
