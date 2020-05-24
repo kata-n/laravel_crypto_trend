4f000c61d232d376e359724dad553fe3ddc604e4
kata-n <kata.kawa813@gmail.com>
2020-05-24 10:33:34 +0900

    t

--- app/Http/Controllers/TwitterAccountController.php
+++ app/Http/Controllers/TwitterAccountController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Abraham\TwitterOAuth\TwitterOAuth;
+use App\User;
 
 class TwitterAccountController extends Controller
 {
