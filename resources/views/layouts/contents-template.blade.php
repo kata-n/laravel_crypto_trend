17bdd60c60930825f2b25cc56ed97476d8599279
kata-n <kata.kawa813@gmail.com>
2020-05-26 08:27:52 +0900

    t

--- resources/views/layouts/contents-template.blade.php
+++ resources/views/layouts/contents-template.blade.php
@@ -14,7 +14,7 @@
     <meta property="og:description" content=""/>
     <meta property="og:image" content="{{ asset('images/top_view2.png') }}"/>
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="./footerFixed.js"></script>
     <title>Crypto Trend | @yield ('title','')</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
   </head>
@@ -36,7 +36,7 @@
       @yield('content')
     </div>
 
-    <footer class="l-footer">
+    <footer id="footer" class="l-footer">
       @yield('footer')
     </footer>
   </body>
