e2052a1abc76611efd6f80088c65d990145f48f3
kata-n <kata.kawa813@gmail.com>
2020-05-31 11:43:20 +0900

    t

--- resources/views/main_page/twitter_account_page.blade.php
+++ resources/views/main_page/twitter_account_page.blade.php
@@ -11,7 +11,6 @@
       <span></span>
       <span></span>
     </div>
-  </nav>
   <nav class="header-nav js-toggle-sp-menu-target" id="js-classtaget">
     <ul class="l-listwrapper p-listwrapper">
       <li class="l-list p-list"><a href="/mainpage">Twitterランキング</a></li>
