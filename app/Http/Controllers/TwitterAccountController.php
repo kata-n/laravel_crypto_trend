f324b336272f4346fc538f4d4a11b4f0c93640f4
kata-n <kata.kawa813@gmail.com>
2020-06-08 01:35:32 +0900

    t

--- app/Http/Controllers/TwitterAccountController.php
+++ app/Http/Controllers/TwitterAccountController.php
@@ -40,7 +40,8 @@ class TwitterAccountController extends Controller
         //teitter_users_listテーブルへAPI取得データを保存する
         foreach($userlists as $userlist => $value){
 
-          $key = in_array("865452033", $registered_list, true);
+//          $key = in_array("865452033", $registered_list, true);
+          $key = $registered_list->contains($value->id);
 
           if($key){
             //IDが登録されているものはteitter_users_listテーブルへAPI取得データを更新する;
