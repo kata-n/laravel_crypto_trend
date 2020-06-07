81a57e3a969cfa6cae37bcd661c8e378716436a4
kata-n <kata.kawa813@gmail.com>
2020-06-08 01:52:35 +0900

    t

--- app/Http/Controllers/TwitterAccountController.php
+++ app/Http/Controllers/TwitterAccountController.php
@@ -35,7 +35,7 @@ class TwitterAccountController extends Controller
         $userlists = $twitter->get('users/search', $params);
 
         //teitter_users_listテーブルのuser_idが既に入っているものを抽出
-        $registered_list = TwitterAccountList::select('twitter_user_id')->get()->pluck('twitter_user_id');
+        $registered_list = TwitterAccountList::select('twitter_user_id')->get()->pluck('twitter_user_id')->toArray();
 
         //teitter_users_listテーブルへAPI取得データを保存する
         foreach($userlists as $userlist => $value){
