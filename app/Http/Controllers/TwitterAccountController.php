d7a3984a0bcb608bbfef1a8bf781fccdd5253c51
kata-n <kata.kawa813@gmail.com>
2020-06-07 10:39:54 +0900

    t

--- app/Http/Controllers/TwitterAccountController.php
+++ app/Http/Controllers/TwitterAccountController.php
@@ -67,10 +67,33 @@ class TwitterAccountController extends Controller
         $request_token =
         \App\TwitterUser::where('user_id', $user['id'])->first();
 
+        //Twitter情報取得
+        $twitter = new TwitterOAuth(
+            config('services.twitter.client_id'),
+            config('services.twitter.client_secret'),
+            $request_token['token'],
+            $request_token['token_secret']
+        );
 
+        //検索クエリ指定
+        $params = array(
+            "q" => "仮想通貨",
+            "lang" => "ja",
+            "locale" => "ja",
+            "count" => "2",
+            "include_entities" => "false",
+        );
+
+        $follower = $twitter->get('users/search', $params);
+        //スクリーンネームだけを取り出す
+        $user_screen_name = array_column($follower,'screen_name');
+        $key= array_rand( $user_screen_name, 1 );
+        $user_screen_name = $user_screen_name[$key];
+
+        $result = $twitter->post('friendships/create', ['screen_name'=> $user_screen_name]);
 
       }
-        return response()->json(['results' => $request_token]);
+        return response()->json(['results' => $result]);
     }
 
     //ログインユーザーの自動フォローONOFFを取得
