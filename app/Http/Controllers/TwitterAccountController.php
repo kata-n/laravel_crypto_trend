e4210bb22e604f96442df0f95241adb66693a480
kata-n <kata.kawa813@gmail.com>
2020-06-07 17:14:42 +0900

    twitter

--- app/Http/Controllers/TwitterAccountController.php
+++ app/Http/Controllers/TwitterAccountController.php
@@ -35,17 +35,17 @@ class TwitterAccountController extends Controller
         $userlists = $twitter->get('users/search', $params);
 
         //teitter_users_listテーブルへAPI取得データを保存する
-//        foreach($userlists as $userlist => $value){
-//          $user_regist = new TwitterAccountList;
-//          $user_regist->twitter_user_id = $value['id'];
-//          $user_regist->account_name = $value['name'];
-//          $user_regist->account_screen_name = $value['screen_name'];
-//          $user_regist->follow_count = $value['friends_count'];
-//          $user_regist->follower_count = $value['status']['text'];
-//          $user_regist->account_description = $value['description'];
-//          $user_regist->account_text = $value['status']['text'];
-//          $user_regist->save();
-//        }
+        foreach($userlists as $userlist => $value){
+          $user_regist = new TwitterAccountList;
+          $user_regist->twitter_user_id = $value->id;
+          $user_regist->account_name = $value['name'];
+          $user_regist->account_screen_name = $value['screen_name'];
+          $user_regist->follow_count = $value['friends_count'];
+          $user_regist->follower_count = $value['status']['text'];
+          $user_regist->account_description = $value['description'];
+          $user_regist->account_text = $value['status']['text'];
+          $user_regist->save();
+        }
 
         //jsonにてVueに渡す
         return response()->json(['results' => $userlists]);
