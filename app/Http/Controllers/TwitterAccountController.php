a65dbf40f269a80d97311d29a573163c721806c0
kata-n <kata.kawa813@gmail.com>
2020-06-08 18:22:11 +0900

    t

--- app/Http/Controllers/TwitterAccountController.php
+++ app/Http/Controllers/TwitterAccountController.php
@@ -125,7 +125,7 @@ class TwitterAccountController extends Controller
         $results = $twitter->get('friends/list', $params);
 
         //TwitterIDだけを取り出す
-        $twitterid_list = array_column($results,'twitter_user_id');
+//        $twitterid_list = array_column($results,'twitter_user_id');
 
         //スクリーンネームだけを取り出す
 //        $user_screen_name = array_column($results,'screen_name');
@@ -145,7 +145,7 @@ class TwitterAccountController extends Controller
 //        }
 
       }
-        return response()->json(['results' => $twitterid_list]);
+        return response()->json(['results' => $results]);
     }
 
     //ログインユーザーの自動フォローONOFFを取得
