7e42c3ab92809d1ce8af62f4732256d34dda9057
kata-n <kata.kawa813@gmail.com>
2020-06-11 20:01:18 +0900

    twitter

--- public/js/script.js
+++ public/js/script.js
@@ -109,14 +109,10 @@ $(function () {
       $('#js-classtaget').removeClass('js-toggle-sp-menu-target');
     }
   }); //フッターを最下部に固定
-
-  var $footer = $('.l-footer');
-
-  if (window.innerHeight > $footer.offset().top + $footer.outerHeight()) {
-    $footer.attr({
-      'style': 'position:fixed; top:' + (window.innerHeight - $footer.outerHeight()) + 'px;'
-    });
-  }
+  //      var $footer = $('.l-footer');
+  //      if(window.innerHeight > $footer.offset().top + $footer.outerHeight() ) {
+  //          $footer.attr({'style': 'position:fixed; top:' + (window.innerHeight - $footer.outerHeight()) + 'px;' });
+  //      }
 });
 
 /***/ }),
--- resources/js/jQuery/script.js
+++ resources/js/jQuery/script.js
@@ -16,9 +16,9 @@ $(function() {
   });
 
   //フッターを最下部に固定
-      var $footer = $('.l-footer');
-      if(window.innerHeight > $footer.offset().top + $footer.outerHeight() ) {
-          $footer.attr({'style': 'position:fixed; top:' + (window.innerHeight - $footer.outerHeight()) + 'px;' });
-      }
+//      var $footer = $('.l-footer');
+//      if(window.innerHeight > $footer.offset().top + $footer.outerHeight() ) {
+//          $footer.attr({'style': 'position:fixed; top:' + (window.innerHeight - $footer.outerHeight()) + 'px;' });
+//      }
 
 });
